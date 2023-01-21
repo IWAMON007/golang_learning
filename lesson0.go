1fa5f7f2abe3b181a85131cbcb78d5c1662d2339
IWAMON007 <123219601+IWAMON007@users.noreply.github.com>
2023-01-21 11:32:54 +0900

    first commit
    初めてのコミット

---
+++ lesson0.go
@@ -0,0 +1,9 @@
+package main /* golang program need to belong to something package. */
+import "fmt" /* Import 'fmt'. Like a ' #includ <stdio.h>' . */
+
+func main(){ /* This is mean 'entry point'. */
+	fmt.Println("Good morning")
+	fmt.Println("Good evening")
+	fmt.Println("Good afternoon")
+	fmt.Println("Good night¥n")
+}
---
+++ lesson1_variable.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+
+func main(){ // Usually don't use 'main' in same folder more than two times but this time is exception.
+
+	var num int
+	num = 1
+
+	var num1 = 100
+
+	num2 := 50
+	Num2 := 55
+
+	fmt.Println(num)
+	fmt.Println(num1)
+	fmt.Println(num2)
+	fmt.Println(Num2)
+
+}
